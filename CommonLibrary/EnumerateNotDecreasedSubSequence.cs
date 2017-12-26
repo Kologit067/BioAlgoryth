b0607a65b958c646b731d298ffbcfc65116a81ae
Oleh Kolomiiets <okolomiiets@10.4.com>
2017-12-26 18:31:45 +0200

    added calculation of Iteration of Count

--- CommonLibrary/EnumerateNotDecreasedSubSequence.cs
+++ CommonLibrary/EnumerateNotDecreasedSubSequence.cs
@@ -62,6 +62,7 @@ namespace CommonLibrary
         /// </summary>		
         protected override bool IsCompleteCondition()
         {
+            fIterationCount++;
             if (fCurrentPosition >= _fSize - 1)
                 return true;
             return false;
