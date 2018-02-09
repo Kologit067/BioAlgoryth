45d0ce23e9fa79aaba0fe01130dc2886f0c44a1c
Oleh Kolomiiets <okolomiiets@10-4.com>
2018-02-09 08:24:15 +0200

    added PreprocessNValueString ...

--- ExactStringCompare/StringPreprocessing.cs
+++ ExactStringCompare/StringPreprocessing.cs
@@ -9,6 +9,11 @@ namespace ExactStringCompare
     public class StringPreprocessing
     {
         protected int[] zValue;
+        //        protected char[] alphabet;
+        protected Dictionary<char, int> rValue;
+        protected Dictionary<char, List<int>> rAdvValue;
+        protected int[] liValue;
+        protected int[] lisValue;
         public void PreprocessString(string line)
         {
             int li = 0;
@@ -39,13 +44,98 @@ namespace ExactStringCompare
                     {
 
                         int j = Ki;
-                        while (line[j-li] == line[j])
+                        while (line[j - li] == line[j])
                             j++;
                         li = i;
                         Ki = j - 1;
                         zValue[i] = Ki - li;
                     }
                 }
+            }
+        }
+
+        public void PreprocessNValueString(string line)
+        {
+            int lastPosition = line.Length;
+            int li = lastPosition;
+            int Ki = lastPosition;
+            zValue = new int[line.Length];
+            for (int i = lastPosition-1; i >= 0; i--)
+            {
+                if (li >= i)
+                {
+                    if (line[i] == line[lastPosition])
+                    {
+                        int j = lastPosition;
+                        while (line[j] == line[i - j])
+                            j--;
+                        li = i;
+                        Ki = j + 1;
+                        zValue[i] = li - Ki;
+                    }
+                }
+                else
+                {
+                    // ---------------------
+                    int i0 = i + li;
+                    if (zValue[i0] - i0 < zValue[li])
+                    {
+                        zValue[i] = zValue[i0];
+                    }
+                    else
+                    {
+
+                        int j = Ki;
+                        while (line[j - li] == line[j])
+                            j++;
+                        li = i;
+                        Ki = j - 1;
+                        zValue[i] = Ki - li;
+                    }
+                }
+            }
+        }
+
+        public void BadSymbolPreprocessString(string line)
+        {
+
+            rAdvValue = new Dictionary<char, List<int>>();
+            for (int i = 1; i < line.Length; i++)
+            {
+                if (!rAdvValue.ContainsKey(line[i]))
+                    rAdvValue.Add(line[i], new List<int>());
+                rAdvValue[line[i]].Add(i);
+            }
+        }
+
+        public void BadSymbolAdvPreprocessString(string line)
+        {
+
+            rValue = new Dictionary<char, int>();
+            for (int i = 1; i < line.Length; i++)
+            {
+                if (rValue.ContainsKey(line[i]))
+                    rValue[line[i]] = i;
+                else
+                    rValue.Add(line[i], i);
+            }
+        }
+
+        public void LiPreprocessString(string line)
+        {
+            int len = line.Length;
+            string lineReverse = line.ToString();
+            lineReverse.Reverse();
+            PreprocessString(lineReverse);
+            rAdvValue = new Dictionary<char, List<int>>();
+            for (int i = len - 1; i >= 0; i--)
+            {
+                lisValue[len - zValue[i] + 1] = len - i - 1;
+            }
+            liValue[1] = lisValue[1];
+            for (int i = 2; i < line.Length; i++)
+            {
+                liValue[1] = Math.Max(lisValue[i-1], lisValue[1]);
             }
         }
     }
