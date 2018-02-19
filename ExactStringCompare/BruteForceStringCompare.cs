b02f61935bb34132c11bce4cb3d2e219ca73c35e
Oleh Kolomiiets <okolomiiets@10.4.com>
2018-02-19 11:57:48 +0200

    completed tests for brute force string compare

--- ExactStringCompare/BruteForceStringCompare.cs
+++ ExactStringCompare/BruteForceStringCompare.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BaseContract;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,21 +10,43 @@ namespace ExactStringCompare
 {
     public class BruteForceStringCompare
     {
+        public static readonly string AlgorythmName = "BFSC";
+        public IStringCompareAccumulator StatisticAccumulator { get; set; }
+
         public List<int> FindSubstring(string text, string pattern)
         {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            StatisticAccumulator.CreateStatistics(text, pattern);
+
             List<int> result = new List<int>();
             int max = text.Length - pattern.Length;
-            for ( int i = 0; i < max; i++)
+            for ( int i = 0; i <= max; i++)
             {
                 int j = 0;
+                int k = i;
+                StatisticAccumulator.IterationCountInc();
                 for (; j < pattern.Length; j++)
                 {
-                    if (text[i] != pattern[j])
+                    StatisticAccumulator.NumberOfComparisonInc();
+                    StatisticAccumulator.IterationCountInc();
+                    if (text[k++] != pattern[j])
                         break;
                 }
+                StatisticAccumulator.IterationCountInc();
                 if (j == pattern.Length)
+                {
+                    StatisticAccumulator.IterationCountInc();
                     result.Add(i);
+                }
             }
+
+            stopwatch.Stop();
+            long elapsedTicks = stopwatch.ElapsedTicks;
+            long durationMilliSeconds = stopwatch.ElapsedMilliseconds;
+            string outputPresentation = string.Join(",", result.Select(p => p.ToString()));
+
+            StatisticAccumulator.SaveStatisticData(outputPresentation, elapsedTicks, durationMilliSeconds, DateTime.Now);
             return result;
         }
     }
--- ExactStringCompareTest/BruteForceStringCompareTest.cs
+++ ExactStringCompareTest/BruteForceStringCompareTest.cs
@@ -12,9 +12,103 @@ namespace ExactStringCompareTest
     [TestClass]
     public class BruteForceStringCompareTest
     {
+        //--------------------------------------------------------------------------------------
+        // multiple testing
+        //--------------------------------------------------------------------------------------
         [TestMethod]
-        public void FindSubstringTest()
+        public void SimpletStringCompareByPreprocessingCharSet2PatterText()
         {
+            // arrange
+            int patternLength = 6;
+            int textLength = 18;
+            char[] alphabet = new char[] { 'a', 'c' };
+            EnumerateCharSetForBruteForceStringCompare enumeration = new EnumerateCharSetForBruteForceStringCompare(
+                alphabet, patternLength, textLength);
+            // act
+            enumeration.Execute();
+            // assert
+
+        }
+        //--------------------------------------------------------------------------------------
+        [TestMethod]
+        public void SimpletStringCompareByPreprocessingCharSet3PatterText()
+        {
+            // arrange
+            int patternLength = 5;
+            int textLength = 10;
+            char[] alphabet = new char[] { 'a', 'c', 'g' };
+            EnumerateCharSetForBruteForceStringCompare enumeration = new EnumerateCharSetForBruteForceStringCompare(
+                alphabet, patternLength, textLength);
+            // act
+            enumeration.Execute();
+            // assert
+
+        }
+        //--------------------------------------------------------------------------------------
+        [TestMethod]
+        public void SimpletStringCompareByPreprocessingCharSet4PatterText()
+        {
+            // arrange
+            int patternLength = 4;
+            int textLength = 7;
+            char[] alphabet = new char[] { 'a', 'c', 'g', 't' };
+            EnumerateCharSetForBruteForceStringCompare enumeration = new EnumerateCharSetForBruteForceStringCompare(
+                alphabet, patternLength, textLength);
+            // act
+            enumeration.Execute();
+            // assert
+
+        }
+        //--------------------------------------------------------------------------------------
+        [TestMethod]
+        public void ProcessInputDataBuAdditionTestLength7Number3Pattern4Step987()
+        {
+            // arrange
+            int step = 98798111;
+            int bufferSize = 1000;
+            int patternLength = 7;
+            int textLength = 14;
+            char[] alphabet = new char[] { 'a', 'c', 'g', 't' };
+            StringCompareAccumulator statisticAccumulator = new StringCompareAccumulator(new StringCompareSaver(), BruteForceStringCompare.AlgorythmName,
+                patternLength, textLength, bufferSize, alphabet.Length);
+            statisticAccumulator.Delete();
+            int size = patternLength + textLength;
+            long max = 1L << (2 * size);
+            long sequenceAsNumber = 0;
+            int[] sequence = new int[size];
+            char[] charSequence = new char[size];
+            long[] masks = new long[size];
+
+            long mask = 3;
+            for (int i = 0; i < size; i++)
+            {
+                masks[i] = mask;
+                mask <<= 2;
+            }
+            // act
+            while (sequenceAsNumber < max)
+            {
+                int shift = 0;
+                for (int i = 0; i < size; i++)
+                {
+                    sequence[i] = (int)((sequenceAsNumber & masks[i]) >> shift);
+                    shift += 2;
+                }
+                sequenceAsNumber += step;
+                charSequence = sequence.Select(j => alphabet[j]).ToArray();
+                string pattern = new string(charSequence.Take(patternLength).ToArray());
+                string text = new string(charSequence.Skip(patternLength).Take(textLength).ToArray());
+                BruteForceStringCompare bruteForceStringCompare = new BruteForceStringCompare()
+                {
+                    StatisticAccumulator = statisticAccumulator
+                };
+                // act
+                bruteForceStringCompare.FindSubstring(text, pattern);
+
+            }
+
+            // assert
+
         }
         //--------------------------------------------------------------------------------------
         // class EnumerateCharSetForBruteForceStringCompare 
@@ -39,7 +133,7 @@ namespace ExactStringCompareTest
                 _textLength = pTextLength;
                 _step = pStep;
                 _stepCounter = 1;
-                _statisticAccumulator = new StringCompareAccumulator(new StringCompareSaver(), SimpletStringCompareByPreprocessing.AlgorythmName,
+                _statisticAccumulator = new StringCompareAccumulator(new StringCompareSaver(), BruteForceStringCompare.AlgorythmName,
                     _patternLength, _textLength, bufferSize, pCharSet.Length);
                 _statisticAccumulator.Delete();
             }
@@ -51,7 +145,10 @@ namespace ExactStringCompareTest
                     var currentSequence = _fCurrentSet.Select(i => _charSet[i]).ToList();
                     string pattern = new string(currentSequence.Take(_patternLength).ToArray());
                     string text = new string(currentSequence.Skip(_patternLength).Take(_textLength).ToArray());
-                    BruteForceStringCompare bruteForceStringCompare = new BruteForceStringCompare();
+                    BruteForceStringCompare bruteForceStringCompare = new BruteForceStringCompare()
+                    {
+                        StatisticAccumulator = _statisticAccumulator
+                    };
                     // act
                     bruteForceStringCompare.FindSubstring(text, pattern);
                     // assert
