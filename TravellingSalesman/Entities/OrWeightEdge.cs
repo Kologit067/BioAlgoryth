5412114689ecfbcdf77be6e95141c807e165bb8a
Oleh Kolomiiets <okolomiiets@10.4.com>
2018-03-22 11:48:02 +0200

    created TSPLazyByEdge

---
+++ TravellingSalesman/Entities/OrWeightEdge.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravellingSalesman.Entities
+{
+    //--------------------------------------------------------------------------------------
+    // class OrWeightEdge 
+    //--------------------------------------------------------------------------------------
+    public class OrWeightEdge
+    {
+        private int _weight;
+        public int Weight
+        {
+            get
+            {
+                return _weight;
+            }
+        }
+        //--------------------------------------------------------------------------------------
+        public OrWeightVertex InVertex { get; set; }
+        //--------------------------------------------------------------------------------------
+        public OrWeightVertex OutVertex { get; set; }
+        //--------------------------------------------------------------------------------------
+        public OrWeightEdge(int weight)
+        {
+            _weight = weight;
+        }
+        //--------------------------------------------------------------------------------------
+        public void Delete()
+        {
+            InVertex.InEdges.Remove(this);
+            OutVertex.OutEdges.Remove(this);
+        }
+        //--------------------------------------------------------------------------------------
+    }
+    //--------------------------------------------------------------------------------------
+}
